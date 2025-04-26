3f1b9be6897ce3aa8554cdf7fb6aaeabeeec7368
jahan <jahan.pd@gmail.com>
2025-04-27 05:59:09 +1000

    cleaned up nvim

--- nvim/lua/configs/harpoon.lua
+++ nvim/lua/configs/harpoon.lua
@@ -1,22 +1,27 @@
 -- Harpoon
+-- local harpoon = require("harpoon")
 
-vim.keymap.set('n', '<leader>hm', function() require("harpoon.mark").add_file() end, opts)
-vim.keymap.set('n', '<leader>hh', function() require("harpoon.ui").toggle_quick_menu() end, opts)
+-- REQUIRED
+-- harpoon:setup()
+-- REQUIRED
 
-local harpoonremap = function(mapping, num)
-  vim.keymap.set('n', mapping, function() require("harpoon.ui").nav_file(num) end, opts)
-end
+-- vim.keymap.set('n', '<leader>hm', function() harpoon:list():add() end)
+-- vim.keymap.set('n', '<leader>hh', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-harpoonremap('<C-a>', 1)
-harpoonremap('<C-s>', 2)
-harpoonremap('<C-d>', 3)
-harpoonremap('<C-f>', 4)
-harpoonremap('<C-g>', 5)
-
-harpoonremap('<C-h>', 6)
-harpoonremap('<C-j>', 7)
-harpoonremap('<C-k>', 8)
-harpoonremap('<C-l>', 9)
-harpoonremap('<C-;>', 10)
-
+-- local harpoonremap = function(mapping, num)
+--   vim.keymap.set('n', mapping, function()  harpoon:list():select(num) end)
+-- end
+--
+-- harpoonremap('<C-a>', 1)
+-- harpoonremap('<C-s>', 2)
+-- harpoonremap('<C-d>', 3)
+-- harpoonremap('<C-f>', 4)
+-- harpoonremap('<C-g>', 5)
+--
+-- harpoonremap('<C-h>', 6)
+-- harpoonremap('<C-j>', 7)
+-- harpoonremap('<C-k>', 8)
+-- harpoonremap('<C-l>', 9)
+-- harpoonremap('<C-;>', 10)
+--
 
--- nvim/lua/plugins/base.lua
+++ nvim/lua/plugins/base.lua
@@ -16,6 +16,7 @@ return {
 	},
 		{
 				'ThePrimeagen/harpoon',
+				branch = "harpoon2",
 				dependencies = {"nvim-lua/plenary.nvim"},
 				config = function()
 						require('../configs.harpoon')
